26d374437ea8ee5913753ef182530d4f290d641b
Nick <nkornien@ucsc.edu>
2022-05-24 00:51:07 -0700

    updated to use better evaluation

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -38,12 +38,12 @@ class CTFAgent(ReflexCaptureAgent):
 
         if self.numInvaders > 1:
             return DefensiveReflexAgent.getFeatures(self, gameState, action)
-        return OffensiveReflexAgent.getFeatures(self, gameState, action)
+        return OffensiveCTFAgent.getFeatures(self, gameState, action)
 
     def getWeights(self, gameState, action):
         if self.numInvaders > 1:
             return DefensiveReflexAgent.getWeights(self, gameState, action)
-        return OffensiveReflexAgent.getWeights(self, gameState, action)
+        return OffensiveCTFAgent.getWeights(self, gameState, action)
 
 
 class CTFAgent2(ReflexCaptureAgent):
@@ -64,97 +64,62 @@ class CTFAgent2(ReflexCaptureAgent):
 
         if self.numInvaders > 0:
             return DefensiveReflexAgent.getFeatures(self, gameState, action)
-        return OffensiveReflexAgent.getFeatures(self, gameState, action)
+        return OffensiveCTFAgent.getFeatures(self, gameState, action)
 
     def getWeights(self, gameState, action):
         if self.numInvaders > 0:
             return DefensiveReflexAgent.getWeights(self, gameState, action)
-        return OffensiveReflexAgent.getWeights(self, gameState, action)
+        return OffensiveCTFAgent.getWeights(self, gameState, action)
 
 
-# class OffensiveReflexAgent(ReflexCaptureAgent):
-#     """
-#     A reflex agent that seeks food.
-#     This agent will give you an idea of what an offensive agent might look like,
-#     but it is by no means the best or only way to build an offensive agent.
-#     """
+class OffensiveCTFAgent(ReflexCaptureAgent):
+    """
+    A reflex agent that seeks food,
+    avoids ghosts at all costs,
+    and eats nearby capsules
+    """
 
-#     def __init__(self, index, **kwargs):
-#         super().__init__(index)
+    def __init__(self, index, **kwargs):
+        super().__init__(index)
 
-#     def getFeatures(self, gameState, action):
-#         features = {}
-#         successor = self.getSuccessor(gameState, action)
-#         features['successorScore'] = self.getScore(successor)
+    def getFeatures(self, gameState, action):
+        features = {}
+        successor = self.getSuccessor(gameState, action)
+        features['successorScore'] = self.getScore(successor)
 
-#         Compute distance to the nearest food.
-#         foodList = self.getFood(successor).asList()
+        # Compute distance to the nearest food.
+        foodList = self.getFood(successor).asList()
 
-#         This should always be True, but better safe than sorry.
-#         if (len(foodList) > 0):
-#             myPos = successor.getAgentState(self.index).getPosition()
-#             minDistance = min([self.getMazeDistance(myPos, food)
-#                               for food in foodList])
-#             features['distanceToFood'] = minDistance
+        if (len(foodList) > 0):
+            myPos = successor.getAgentState(self.index).getPosition()
+            minDistance = min([self.getMazeDistance(myPos, food)
+                              for food in foodList])
+            features['distanceToFood'] = minDistance
 
-#         return features
+        # avoid ghosts at all costs
+        enemies = [successor.getAgentState(i)
+                   for i in self.getOpponents(successor)]
+        ghosts = [a for a in enemies if not a.isPacman() and a.getPosition()
+                  is not None]
+        ghostDistances = [self.getMazeDistance(
+            myPos, ghost._position) for ghost in ghosts]
 
-#     def getWeights(self, gameState, action):
-#         return {
-#             'successorScore': 100,
-#             'distanceToFood': -1
-#         }
+        features['ghostProximity'] = 0
+        for ghostDistance in ghostDistances:
+            if ghosts[0]._scaredTimer:
+                break
+            if ghostDistance > 1:
+                features['ghostProximity'] = 1
 
+        # give a bonus for eating capsules
+        features['capsulesRemaining'] = len(successor.getCapsules())
 
-# class DefensiveReflexAgent(ReflexCaptureAgent):
-#     """
-#     A reflex agent that tries to keep its side Pacman-free.
-#     This is to give you an idea of what a defensive agent could be like.
-#     It is not the best or only way to make such an agent.
-#     """
+        return features
 
-#     def __init__(self, index, **kwargs):
-#         super().__init__(index)
-
-#     def getFeatures(self, gameState, action):
-#         features = {}
-
-#         successor = self.getSuccessor(gameState, action)
-#         myState = successor.getAgentState(self.index)
-#         myPos = myState.getPosition()
-
-#         Computes whether we're on defense (1) or offense (0).
-#         features['onDefense'] = 1
-#         if (myState.isPacman()):
-#             features['onDefense'] = 0
-
-#         Computes distance to invaders we can see.
-#         enemies = [successor.getAgentState(i)
-#                    for i in self.getOpponents(successor)]
-#         invaders = [a for a in enemies if a.isPacman(
-#         ) and a.getPosition() is not None]
-#         features['numInvaders'] = len(invaders)
-
-#         if (len(invaders) > 0):
-#             dists = [self.getMazeDistance(
-#                 myPos, a.getPosition()) for a in invaders]
-#             features['invaderDistance'] = min(dists)
-
-#         if (action == Directions.STOP):
-#             features['stop'] = 1
-
-#         rev = Directions.REVERSE[gameState.getAgentState(
-#             self.index).getDirection()]
-#         if (action == rev):
-#             features['reverse'] = 1
-
-#         return features
-
-#     def getWeights(self, gameState, action):
-#         return {
-#             'numInvaders': -1000,
-#             'onDefense': 100,
-#             'invaderDistance': -10,
-#             'stop': -100,
-#             'reverse': -2
-#         }
+    def getWeights(self, gameState, action):
+        return {
+            'successorScore': 100,
+            'distanceToFood': -1,
+            'ghostProximity': -999999,
+            'capsulesRemaining': -50
+        }
