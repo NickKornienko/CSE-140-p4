3d3e545bb44bebcee019de329740888f24f5e88b
Nick <nkornien@ucsc.edu>
2022-05-24 14:17:54 -0700

    updated defense agent and ghosat avoidance

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -1,5 +1,5 @@
-from pacai.agents.capture.defense import DefensiveReflexAgent
 from pacai.agents.capture.reflex import ReflexCaptureAgent
+from pacai.core.directions import Directions
 
 
 def createTeam(firstIndex, secondIndex, isRed):
@@ -11,7 +11,7 @@ def createTeam(firstIndex, secondIndex, isRed):
     """
 
     firstAgent = CTFAgent
-    secondAgent = CTFAgent2
+    secondAgent = DefensiveCTFAgent
 
     return [
         firstAgent(firstIndex),
@@ -36,38 +36,12 @@ class CTFAgent(ReflexCaptureAgent):
         self.numInvaders = len(invaders)
 
         if self.numInvaders > 1:
-            return DefensiveReflexAgent.getFeatures(self, gameState, action)
+            return DefensiveCTFAgent.getFeatures(self, gameState, action)
         return OffensiveCTFAgent.getFeatures(self, gameState, action)
 
     def getWeights(self, gameState, action):
         if self.numInvaders > 1:
-            return DefensiveReflexAgent.getWeights(self, gameState, action)
-        return OffensiveCTFAgent.getWeights(self, gameState, action)
-
-
-class CTFAgent2(ReflexCaptureAgent):
-    """
-    Hybrid agent that will attack unless there are invaders
-    """
-
-    def __init__(self, index, **kwargs):
-        self.numInvaders = 0
-        super().__init__(index)
-
-    def getFeatures(self, gameState, action):
-        enemies = [gameState.getAgentState(i)
-                   for i in self.getOpponents(gameState)]
-        invaders = [a for a in enemies if a.isPacman(
-        ) and a.getPosition() is not None]
-        self.numInvaders = len(invaders)
-
-        if self.numInvaders > 0:
-            return DefensiveReflexAgent.getFeatures(self, gameState, action)
-        return OffensiveCTFAgent.getFeatures(self, gameState, action)
-
-    def getWeights(self, gameState, action):
-        if self.numInvaders > 0:
-            return DefensiveReflexAgent.getWeights(self, gameState, action)
+            return DefensiveCTFAgent.getWeights(self, gameState, action)
         return OffensiveCTFAgent.getWeights(self, gameState, action)
 
 
@@ -96,21 +70,21 @@ class OffensiveCTFAgent(ReflexCaptureAgent):
             features['distanceToFood'] = minDistance
 
         # avoid ghosts at all costs unless scared
-        # enemies = [successor.getAgentState(i)
-        #            for i in self.getOpponents(successor)]
-        # ghosts = [a for a in enemies if not a.isPacman() and a.getPosition()
-        #           is not None]
-        # ghostDistances = [self.getMazeDistance(
-        #     myPos, ghost._position) for ghost in ghosts]
+        enemies = [successor.getAgentState(i)
+                   for i in self.getOpponents(successor)]
+        ghosts = [a for a in enemies if not a.isPacman() and a.getPosition()
+                  is not None]
+        ghostDistances = [self.getMazeDistance(
+            myPos, ghost._position) for ghost in ghosts]
 
         features['ghostProximity'] = 0
         features['scaredGhostProximity'] = 0
-        # for ghostDistance in ghostDistances:
-        #     if ghostDistance > 1:
-        #         if ghosts[0]._scaredTimer > 0:
-        #             features['scaredGhostProximity'] = 1
-        #         else:
-        #             features['ghostProximity'] = 1
+        for ghostDistance in ghostDistances:
+            if ghostDistance > 1:
+                if ghosts[0]._scaredTimer > 0:
+                    features['scaredGhostProximity'] = 1
+                else:
+                    features['ghostProximity'] = 1
 
         # give a bonus for eating capsules
         features['capsulesRemaining'] = len(successor.getCapsules())
@@ -124,4 +98,62 @@ class OffensiveCTFAgent(ReflexCaptureAgent):
             'ghostProximity': -999999,
             'capsulesRemaining': -50,
             'scaredGhostProximity': 999999
+        }
+
+
+class DefensiveCTFAgent(ReflexCaptureAgent):
+    """
+    A reflex agent that tries to keep its side Pacman-free.
+    If no invaders are present it will move towards the middle
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index)
+
+    def getFeatures(self, gameState, action):
+        features = {}
+
+        successor = self.getSuccessor(gameState, action)
+        myState = successor.getAgentState(self.index)
+        myPos = myState.getPosition()
+
+        # Computes whether we're on defense (1) or offense (0).
+        features['onDefense'] = 1
+        if (myState.isPacman()):
+            features['onDefense'] = 0
+
+        # Computes distance to invaders we can see.
+        enemies = [successor.getAgentState(i)
+                   for i in self.getOpponents(successor)]
+        invaders = [a for a in enemies if a.isPacman(
+        ) and a.getPosition() is not None]
+        features['numInvaders'] = len(invaders)
+
+        # Go after invaders, or approach enemies on the other side
+        if (len(invaders) > 0):
+            dists = [self.getMazeDistance(
+                myPos, a.getPosition()) for a in invaders]
+            features['invaderDistance'] = min(dists)
+        else:
+            dists = [self.getMazeDistance(
+                myPos, a.getPosition()) for a in enemies]
+            features['invaderDistance'] = min(dists)
+
+        if (action == Directions.STOP):
+            features['stop'] = 1
+
+        rev = Directions.REVERSE[gameState.getAgentState(
+            self.index).getDirection()]
+        if (action == rev):
+            features['reverse'] = 1
+
+        return features
+
+    def getWeights(self, gameState, action):
+        return {
+            'numInvaders': -1000,
+            'onDefense': 100,
+            'invaderDistance': -10,
+            'stop': -100,
+            'reverse': -2
         }
